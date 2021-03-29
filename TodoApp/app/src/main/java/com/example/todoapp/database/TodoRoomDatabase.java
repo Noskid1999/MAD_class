625dcd3d7b66acad286720aa003a2ce16982d3c3
noskid7049 <diksonrajbanshi15@gmail.com>
2021-03-29 08:30:06 +0545

    Added Dummy db populate

--- TodoApp/app/src/main/java/com/example/todoapp/database/TodoRoomDatabase.java
+++ TodoApp/app/src/main/java/com/example/todoapp/database/TodoRoomDatabase.java
@@ -1,13 +1,18 @@
 package com.example.todoapp.database;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.todoapp.database.dao.TodoDAO;
 import com.example.todoapp.database.model.ETodo;
+
+import java.util.Date;
 
 @Database(entities = {ETodo.class}, version = 1, exportSchema = false)
 public abstract class TodoRoomDatabase extends RoomDatabase {
@@ -19,8 +24,10 @@ public abstract class TodoRoomDatabase extends RoomDatabase {
             synchronized (TodoRoomDatabase.class) {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(), TodoRoomDatabase.class, DB_NAME)
-                            .allowMainThreadQueries().fallbackToDestructiveMigration().build();
-
+                            .allowMainThreadQueries()
+                            .fallbackToDestructiveMigration()
+                            .addCallback(sCallback)
+                            .build();
                 }
             }
         }
@@ -29,5 +36,28 @@ public abstract class TodoRoomDatabase extends RoomDatabase {
 
     public abstract TodoDAO mTodoDAO();
 
+    public static class populateDbAsync extends AsyncTask<ETodo, Void, Void> {
+        private TodoDAO mTodoDAO;
+
+        private populateDbAsync(TodoRoomDatabase db) {
+            this.mTodoDAO = db.mTodoDAO();
+        }
+
+        @Override
+        protected Void doInBackground(ETodo... eTodos) {
+            Date date = new Date();
+            ETodo eTodo = new ETodo("Demo Title", "Demo Description", 1, 0, date);
+            mTodoDAO.insert(eTodo);
+            return null;
+        }
+    }
+
+    private static RoomDatabase.Callback sCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new populateDbAsync(INSTANCE).execute();
+        }
+    };
 
 }
